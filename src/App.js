394f06692e07c5f192528d953ece067a4356d8aa
Bhavyaratra <bhavya.r1220@gmail.com>
2022-04-10 14:54:46 +0530

    refactored inital data request

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
+import { useEffect, useState } from "react";
+import Loader from "./util/Loader";
 import Main from "./views/Main";
 
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+
+  async function setInitialData() {
+    try {
+      const res = await fetch(
+        "https://movie-task.vercel.app/api/popular?page=1"
+      );
+      const resJson = await res.json();
+      setMovies(resJson.data.results);
+      setIsLoading(false);
+    } catch (error) {
+      setIsLoading(false);
+      setError("server error");
+      console.log("error while fetching", error);
+    }
+  }
+
+  useEffect(() => {
+    setInitialData();
+  }, []);
+
   return (
-    <div >
-      <Main/>
+    <div>
+      {isLoading ? <Loader /> : <Main initMovies={movies} />}
+      {error && <p className="text-danger">{error}</p>}
     </div>
   );
 }
